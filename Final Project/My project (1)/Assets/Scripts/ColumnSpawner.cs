ba620301bd66fdd542e39e434b7f68dffe6680ab
Guilherme dos Santos <gui.martinson@gmail.com>
2024-10-24 16:26:54 -0400

    Final Project | Game Mechanics

---
+++ Final Project/My project (1)/Assets/Scripts/ColumnSpawner.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnSpawner : MonoBehaviour
+{
+    public GameObject[] leftColumnPrefabs;  
+    public GameObject[] rightColumnPrefabs; 
+    public float spawnRate = 2f;            
+    public float columnSpeed = 5f;         
+    public Transform leftSpawnPoint;        
+    public Transform rightSpawnPoint;       
+
+    private bool spawnOnLeft = true;        
+
+    private void Start()
+    {
+        StartCoroutine(SpawnColumns());     
+    }
+
+    IEnumerator SpawnColumns()
+    {
+        while (true)
+        {
+            if (spawnOnLeft)
+            {
+                SpawnColumn(leftColumnPrefabs, leftSpawnPoint);  
+            }
+            else
+            {
+                SpawnColumn(rightColumnPrefabs, rightSpawnPoint); 
+            }
+
+            spawnOnLeft = !spawnOnLeft;      
+            yield return new WaitForSeconds(spawnRate);  
+        }
+    }
+
+    void SpawnColumn(GameObject[] columnPrefabs, Transform spawnPoint)
+    {
+        
+        GameObject columnPrefab = columnPrefabs[Random.Range(0, columnPrefabs.Length)];
+
+        
+        GameObject column = Instantiate(columnPrefab, spawnPoint.position, Quaternion.identity);
+
+        // Move column down
+        StartCoroutine(MoveColumnDown(column));
+    }
+
+    IEnumerator MoveColumnDown(GameObject column)
+    {
+        while (column.transform.position.y > -10)  
+        {
+            column.transform.Translate(Vector3.down * columnSpeed * Time.deltaTime);
+            yield return null;  
+        }
+
+        
+        Destroy(column);
+    }
+}
--- Final Project/My project (1)/Assets/Scripts/GameManager.cs
+++ Final Project/My project (1)/Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public float gravityForce = 9.81f; // Force applied for gravity
     private bool gravityToLeft = true; // Start with gravity pulling left
+    public GameObject player; // Reference to the player object
 
     void Start()
     {
@@ -15,6 +16,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         HandleGravitySwitch();
+        CheckPlayerBounds(); // Check if player is out of bounds
     }
 
     // Switch gravity direction based on space bar press
@@ -38,5 +40,26 @@ public class GameManager : MonoBehaviour
         {
             Physics2D.gravity = new Vector2(gravityForce, 0); // Gravity pulls to the right
         }
+    }
+
+    // Check if the player goes out of the screen bounds
+    void CheckPlayerBounds()
+    {
+        
+        Vector3 playerViewportPosition = Camera.main.WorldToViewportPoint(player.transform.position);
+
+        
+        if (playerViewportPosition.x < 0 || playerViewportPosition.x > 1 || playerViewportPosition.y < 0 || playerViewportPosition.y > 1)
+        {
+            PlayerDied(); 
+        }
+    }
+
+    
+    void PlayerDied()
+    {
+        
+        Debug.Log("Player died!"); 
+        
     }
 }
