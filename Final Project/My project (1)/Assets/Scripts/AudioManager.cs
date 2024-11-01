03a8e6a9bd7c914715d66ad924ec2202267f7204
Guilherme dos Santos <gui.martinson@gmail.com>
2024-10-31 23:23:57 -0400

    Final Project | Background Music and Sounds

---
+++ Final Project/My project (1)/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+// AudioManager.cs
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip backgroundMusic;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = backgroundMusic;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
